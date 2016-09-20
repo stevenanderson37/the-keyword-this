bc3bc7a56f2857f6375cf93a2fcad0282642ec6a
Steven Anderson <stevenanderson@Stevens-MacBook-Pro.local>
2016-09-20 14:52:27 -0600

    partially complete

--- practice.js
+++ practice.js
@@ -2,20 +2,27 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+      It allows the code to be cleaner by allowing a shortcut to the code it is being executed in by looking at the context of where it is being called.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
+/*
+      1. Explicit: told exactly where the this keyword is coming from through the use of .call, .apply, or .bind.
+      2. Implicit: also known as "left of the dot," when the context is not explicity told it looks for the implied context, which would be found to the left of the dot. For example, o.average(); would look for the context of "this" in the variable "o".
+      3. Default/window: when the context of the this keyword cannot be found explicitly or implicitly then it is taken out to the window, which is a bad thing that we never want to happen.
+      4. New/constructor: gives us the ability to create new objects through the use of the this keyword in a constructor function that we can then execute the function with new data to create a new object. It is good practice to always capitalize the first letter of a constructor function to indicate to everyone that it is not a normal function, and that it can only be used as a constructor.
 
   // 3) What is the difference between call and apply?
 
       //Answer
+      Call and apply can be used pretty much interchangeably. They both happen immediately after they are called, and the order of the parameters matter in both as the parameters are called in the order they are given. However, call uses normal parameters while apply can use an array.
 
   // 4) What does .bind do?
 
       //Answer
-
-
+      .bind binds to a function and creates a new function with a unique name.
+*/
 //Next Problem
 
 //Create an object called user which has the following properties.
@@ -24,6 +31,13 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+var user = {
+  username: ,
+  email: ,
+  getUsername: function(){
+    return 
+  }
+}
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
